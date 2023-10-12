6192cc92dd87517b374cddd0197a4e1290b3932c
filnow <filnow3@st.amu.edu.pl>
2023-10-12 16:29:38 +0200

    add tailwind

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'jd-clone-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
