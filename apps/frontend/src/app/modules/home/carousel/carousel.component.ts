6285caea436c9448aff300e6d2966939a7076100
filnow <filnow3@st.amu.edu.pl>
2023-10-17 15:03:16 +0200

    fix carousel

--- apps/frontend/src/app/modules/home/carousel/carousel.component.ts
+++ apps/frontend/src/app/modules/home/carousel/carousel.component.ts
@@ -13,18 +13,21 @@ import {RouterModule} from '@angular/router'
 })
 export class CarouselComponent implements OnInit {
 
-  slides = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
+  slides = new Array(4).fill({id: -1, src: '', title: '', subtitle: ''});
 
   ngOnInit(): void {
     this.slides[0] = {
-      src: '../../../assets/images/Mobile_Top_Banner_720x1050 (1).avif',
+      src: '../../../assets/images/Desktop_Top_Banner_1920x840.avif',
     };
     this.slides[1] = {
-      src: '../../../assets/images/Mobile_Top_Banner_720x1050.avif',
+      src: '../../../assets/images/Desktop_Top_Banner_1920x840_HOME.avif',
     }
     this.slides[2] = {
       src: '../../../assets/images/clothes/3721324_CAM_Culture_Cred_JD_PL_EN_FW23_1920x840_v2.avif',
    
+    }
+    this.slides[3] = {
+      src: '../../../assets/images/Desktop_Top_Banner_1920x840_pink.avif',
     }
   }
 }
