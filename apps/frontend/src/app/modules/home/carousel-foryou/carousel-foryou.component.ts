0b8566a526e61c48c9fc8d60bc1598c11ce7d9d8
filnow <filnow3@st.amu.edu.pl>
2023-10-18 21:17:17 +0200

    fix header

---
+++ apps/frontend/src/app/modules/home/carousel-foryou/carousel-foryou.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'homepage-carousel-foryou',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './carousel-foryou.component.html',
+  styleUrls: ['./carousel-foryou.component.css'],
+})
+export class CarouselForyouComponent {}
--- apps/frontend/src/app/modules/home/home.component.ts
+++ apps/frontend/src/app/modules/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselComponent } from './carousel/carousel.component';
+import { CarouselForyouComponent } from './carousel-foryou/carousel-foryou.component';
 
 @Component({
   selector: 'home-page',
   standalone: true,
-  imports: [CommonModule, CarouselComponent],
+  imports: [CommonModule, CarouselComponent, 
+            CarouselForyouComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
