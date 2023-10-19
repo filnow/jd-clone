d09a48bf5454cfa91808c91aace7061a614eb980
filnow <filnow3@st.amu.edu.pl>
2023-10-19 14:23:36 +0200

    add for you

--- apps/frontend/src/app/modules/home/carousel-foryou/carousel-foryou.component.ts
+++ apps/frontend/src/app/modules/home/carousel-foryou/carousel-foryou.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
+interface CarouselItem {
+  picture: string;
+  price: string;
+  name: string;
+}
+
 @Component({
   selector: 'homepage-carousel-foryou',
   standalone: true,
@@ -8,4 +15,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './carousel-foryou.component.html',
   styleUrls: ['./carousel-foryou.component.css'],
 })
-export class CarouselForyouComponent {}
+export class CarouselForyouComponent {
+
+  carouselList: Array<CarouselItem> = [
+    {
+      picture: '../../../assets/images/shoes/jd_120181_al.jpeg',
+      price: '399,99zł',
+      name: 'Converse Chuck Taylor All Star Lift',
+    },
+    {
+      picture: '../../../assets/images/shoes/jd_556251_al.jpeg',
+      price: '319,99zł',
+      name: 'Nike Bluza Z Kapturem W',
+    },
+    {
+      picture: '../../../assets/images/shoes/jd_669147_al.jpeg',
+      price: '329,99zł',
+      name: 'Nike Bluza Z Kapturem W'
+    },
+    {
+      picture: '../../../assets/images/shoes/jd_556004_al.jpeg',
+      price: '319,99zł',
+      name: 'Nike Bluza Z Kapturem Os' 
+    }
+
+  ]
+}
